8f9ceceb2110cbec8926972c84f5e03124d97427
Hnin <hninyati3106@gmail.com>
2025-07-22 16:25:24 +0630

    login verify is partially finished

--- admin/adminLogin.php
+++ admin/adminLogin.php
@@ -1,10 +1,35 @@
 <?php 
-// login from buttom name 
+// login from button name 
+require_once "dbconnect.php";
+
 if(isset($_POST['login'])) // $_POST is super global array
+{
+    $email = $_POST['email'];  // retrieve email value of user 
+    $password = $_POST['password']; // retrieve password of user
+    
+    // echo "email is $email and password is $password."; (debugging)
+    $sql = "SELECT * FROM admin WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$email]);
+    $adminInfo = $stmt->fetch(); // single row returns
+
+    // $adminInfo ['id'], $adminInfo ['email'], $adminInfo ['password'], $adminInfo ['remark']
+
+    if($adminInfo)
     {
-       $email = $_POST['email'];  // retrieve email value of user 
-       $password = $_POST['password']; //retrieve password of user
-        // echo "email is $email and password is $password."; (debugging)
+        if(password_verify($password, $adminInfo['password'])) // checks password and hash match
+        {
+            echo "login success!!";
+        }
+        else
+        {
+            $errorMessage = "Email or Password might be incorrect!";
+        }
+    }
+    else
+    {
+        $errorMessage = "Email or Password might be incorrect!";
+    }
 }
 ?>
 
@@ -15,7 +40,7 @@ if(isset($_POST['login'])) // $_POST is super global array
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container-fluid">
@@ -26,19 +51,24 @@ if(isset($_POST['login'])) // $_POST is super global array
         </div>
         <div class="row">
             <div class="col-md-6 mx-auto py-5">
-            <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-                <div class="mb-3">
-                    <label for="" class="form-label">Email</label>
-                    <input type="email" class="form-control" name="email">
-                </div>
-                <div class="mb-3">
+                <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+                    <?php 
+                    if(isset($errorMessage)){
+                        echo "<p class='alert alert-danger'>$errorMessage</p>";
+                    }
+                    ?>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                    <div class="mb-3">
                         <label for="">Password</label>
                         <input type="password" class="form-control" name="password">
-                </div>
-                <button type="submit" class="btn btn-outline-primary" name="login">
-                    Login
-                </button>
-            </form>
+                    </div>
+                    <button type="submit" class="btn btn-outline-primary" name="login">
+                        Login
+                    </button>
+                </form>
             </div>
         </div>
     </div>
--- admin/dbconnect.php
+++ admin/dbconnect.php
@@ -10,7 +10,7 @@ try {
     $conn = new PDO($dsn,$username,$password); //$dsn, $user, $password
     $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $conn -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-    echo "connection got";
+    // echo "connection got";
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
